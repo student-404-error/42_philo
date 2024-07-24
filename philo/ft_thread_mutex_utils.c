0dd54b1966eb56d8ddf94bb1fc679fd93544f090
student-404-error <ksh30102@gmail.com>
2024-07-24 22:18:56 +0200

    separate utils

---
+++ philo/ft_thread_mutex_utils.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_thread_mutex_utils.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seong-ki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 21:09:27 by seong-ki          #+#    #+#             */
+/*   Updated: 2024/07/24 21:09:58 by seong-ki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	ft_join_thread(int num_of_philo, t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < num_of_philo)
+	{
+		pthread_join(philo[i].thread, NULL);
+		i++;
+	}
+}
+
+void	ft_destroy_mutex(int num_of_philo, pthread_mutex_t *fork)
+{
+	int	i;
+
+	i = 0;
+	while (i < num_of_philo)
+	{
+		pthread_mutex_destroy(&fork[i]);
+		i++;
+	}
+}
+
+void	ft_free_thread_mutex(t_philo *philo, pthread_mutex_t *fork)
+{
+	ft_destroy_mutex(arg.num_of_philo, fork);
+	free(fork);
+	free(philo);
+}
