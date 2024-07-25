0a209351ea41fa8a16a431f87446c660f6189e99
student-404-error <ksh30102@gmail.com>
2024-07-25 19:44:25 +0200

    unused this file

--- philo/philo.c
+++ philo/philo.c
@@ -6,10 +6,10 @@
 /*   By: seong-ki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 19:28:15 by seong-ki          #+#    #+#             */
-/*   Updated: 2024/07/23 20:44:11 by seong-ki         ###   ########.fr       */
+/*   Updated: 2024/07/25 16:23:09 by seong-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+/*
 #include "philo.h"
 
 pthread_mutex_t	mtx;
@@ -45,7 +45,7 @@ int	main(int ac, char *av[])
 	(void) av;
 	(void) ac;
 	int	shared_var = 0;
-/*
+
 	struct timeval tv;
 	time_t	current_time;
 
@@ -53,7 +53,7 @@ int	main(int ac, char *av[])
 	current_time = tv.tv_sec;
 	if (ac != 5 && ac != 6)
 		printf("%d %d %d\n", tv.tv_hour, );
-*/
+
 	// thread 핸들러
 	pthread_t	thread1;
 	pthread_t	thread2;
@@ -75,13 +75,13 @@ int	main(int ac, char *av[])
 		printf("The thread could not be detached.\n");
 		exit(2);
 	}
-	/*
+	
 	result = pthread_join(thread, NULL);
 	if (result)
 	{
 		printf("The thread could not be joined. Error number: %d\n", result);
 		exit(2);
-	}*/
+	}
 	pthread_mutex_destroy(&mtx);
 	return (0);
-}
+}*/
