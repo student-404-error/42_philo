ef9850ffdbad8335e256700ac52864f48a729f5e
student-404-error <ksh30102@gmail.com>
2024-07-25 18:31:36 +0200

    Create print, get_ms

--- philo/main.c
+++ philo/main.c
@@ -6,23 +6,69 @@
 /*   By: seong-ki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 22:06:17 by seong-ki          #+#    #+#             */
-/*   Updated: 2024/07/24 21:10:29 by seong-ki         ###   ########.fr       */
+/*   Updated: 2024/07/25 18:29:14 by seong-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <string.h>
 
+int	ft_print_philo_state(t_arg *arg, t_philo *philo, char *state)
+{
+	long long	now;
+
+	now = ft_get_ms();
+	if (now == -1)
+		return (-1);
+	pthread_mutex_lock(&(arg->print));
+	if (!(arg->finish))
+		printf("%lld %d %s\n", now - arg->start_time, philo->id + 1, state);
+	pthread_mutex_unlock(&(arg->print));
+	return (0);
+}
+
+long long	ft_get_ms(void)
+{
+	struct timeval	tv;
+
+	if (!gettimeofday(&tv, NULL))
+		return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+	return (-1);
+}
+
+void	ft_pass_time(long long wait_time, t_arg *arg)
+{
+	long long	start;
+	long long	now;
+
+	start = ft_get_ms();
+	while (!(arg->finish))
+	{
+		now = ft_get_ms();
+		if ((now - start) >= wait_time)
+			break ;
+		usleep(10);
+	}
+}
+
 void	*philo_routine(void *arg)
 {
+	t_arg	*args;
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
+	args = philo->arg;
 	// mutex Lock / use avoid deadlock algo
 	pthread_mutex_lock(philo->left_fork);
 	pthread_mutex_lock(philo->right_fork);
-//	eating(philo);
-	usleep(10000);
+
+	ft_pass_time(args->time_to_eat, args);
+	ft_print_philo_state(args, philo, "eating");
+	// 먹기
+	// 자기
+	// 생각하기
+	
+
 	// mutex Unlock
 	pthread_mutex_unlock(philo->left_fork);
 	pthread_mutex_unlock(philo->right_fork);
@@ -30,7 +76,8 @@ void	*philo_routine(void *arg)
 }
 
 int	ft_errno(int errno, char *loc)
-{ if (errno == 2)
+{
+	if (errno == 2)
 		printf("Invalid Argument: %s\n", loc);
 	else if (errno == 3)
 		printf("pthread Error: %s\n", loc);
@@ -38,7 +85,7 @@ int	ft_errno(int errno, char *loc)
 		printf("malloc Error: %s\n", loc);
 	else
 		printf("etc Error: %s\n", loc);
-	return (FAILURE);
+	return (errno);
 }
 
 int	main(int ac, char *av[])
@@ -61,6 +108,6 @@ int	main(int ac, char *av[])
 	if (errno)
 		return (ft_errno(errno, "ft_init_philo"));
 	ft_join_thread(arg.num_of_philo, philo);
-	ft_free_thread_mutex(fork, philo);
+	ft_free_thread_mutex(arg.num_of_philo, philo, fork);
 	return (SUCCESS);
 }
